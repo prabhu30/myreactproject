7929e94e40584d0310596042b33c7d73fc5842ef
prabhu30 <prabhukalyan30@gmail.com>
2020-10-08 20:15:16 +0530

    Updates

---
+++ precache-manifest.d74762aaa253a5f32db881ec72c87fda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aabaccc9bb3c5baea826c2a9d88b3cc7",
+    "url": "/myreactproject/index.html"
+  },
+  {
+    "revision": "7d81080868c69b631183",
+    "url": "/myreactproject/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7c959d4c9a7ee6cc90cc",
+    "url": "/myreactproject/static/js/2.26731ef1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreactproject/static/js/2.26731ef1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d81080868c69b631183",
+    "url": "/myreactproject/static/js/main.f6690fa4.chunk.js"
+  },
+  {
+    "revision": "efb550db1affb0cdbd31",
+    "url": "/myreactproject/static/js/runtime-main.0ccbf96d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/myreactproject/static/media/logo.5d5d9eef.svg"
+  }
+]);
